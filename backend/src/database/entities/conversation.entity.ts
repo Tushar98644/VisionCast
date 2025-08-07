15ba9da752e61239616a12e0fe0e3eb92d10300d
Tushar Banik <b.toshar@iitg.ac.in>
2025-08-07 22:58:54 +0530

    conversation entity

---
+++ backend/src/database/entities/conversation.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Message } from './message.entity';
+
+@Entity('conversations')
+export class Conversation {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 255 })
+  title: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => Message, (message) => message.conversation, {
+    cascade: true,
+  })
+  messages: Message[];
+}
